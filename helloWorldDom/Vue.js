7de7db5041ab375708483d15fcbf22618feeffc6
Gustavo Pereira <gustavogustavo@protonmail.com>
2019-09-22 20:13:49 -0300

    Hello World and some other stuff with DOM

---
+++ helloWorldDom/Vue.js
@@ -0,0 +1,17 @@
+new Vue({
+  el: "#app",
+  data: {
+  	title: "hello, world!", 
+    now: Date.now(),
+    htmlExample: "<h2>hey</h2>"
+   
+  },n
+  methods: {
+  	nowDate() {
+    	return Date.now();
+    }
+  }
+});
+
+
+
